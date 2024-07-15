3b08d553a81d579ef68704ae94074db71cc45a5a
ic5594 <ic5594@naver.com>
2024-07-15 12:27:11 +0900

    로그인 상황에 따른 ui 변경

--- src/pages/Authentication.js
+++ src/pages/Authentication.js
@@ -1,4 +1,5 @@
 import { json, redirect } from 'react-router-dom';
+
 import AuthForm from '../components/AuthForm';
 
 function AuthenticationPage() {
@@ -7,36 +8,44 @@ function AuthenticationPage() {
 
 export default AuthenticationPage;
 
-export async function action({request}) {
-  console.log(request)
+export async function action({ request }) {
   const searchParams = new URL(request.url).searchParams;
-  const mode = searchParams.get('mode') || 'login'
+  const mode = searchParams.get('mode') || 'login';
 
-  if(mode !== 'login' && mode !== 'signup') {
-    throw json({message: 'Unsupported mode.'}, {status: 422})
+  if (mode !== 'login' && mode !== 'signup') {
+    throw json({ message: 'Unsupported mode.' }, { status: 422 });
   }
 
-  const data = request.formData();
+  const data = await request.formData();
   const authData = {
     email: data.get('email'),
-    password: data.get('password')
-  }
+    password: data.get('password'),
+  };
 
-  const response = fetch('http://localhost:8080/' + mode, {
+  const response = await fetch('http://localhost:8080/' + mode, {
     method: 'POST',
     headers: {
-      'Content-type': 'application/json'
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify(authData)
-  })
+    body: JSON.stringify(authData),
+  });
 
   if (response.status === 422 || response.status === 401) {
     return response;
   }
 
-  if(!response.ok) {
-    throw json({message: 'Could not authenticate user'}, {status: 500})
+  if (!response.ok) {
+    throw json({ message: 'Could not authenticate user.' }, { status: 500 });
   }
 
-  return redirect('/')
+  const resData = await response.json();
+  console.log(resData, '@@  ')
+  const token = resData.token;
+
+  localStorage.setItem('token', token)
+  const expiration = new Date();
+  expiration.setHours(expiration.getHours + 1);
+  localStorage.setItem('expiration', expiration.toISOString())
+
+  return redirect('/');
 }
